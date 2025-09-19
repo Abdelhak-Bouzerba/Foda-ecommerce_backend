db5d69371095aa39e6c34938b7130d3244a98dda
Abdelhak-Bouzerba <abdelhakbouzerba6@gmail.com>
2025-09-19 17:01:33 +0100

    setup structure for testing with jest

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testMatch: ["**/tests/**/*.test.ts"], // all test files
+  moduleFileExtensions: ["ts", "js", "json", "node"],
+};
